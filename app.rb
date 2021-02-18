cd67f79dcb269d06bf03130e032676cb1a89d588
RowHunt <watchlist.rh@gmail.com>
2021-02-18 13:33:25 -0500

    Test passed, refactoring done and more

--- app.rb
+++ app.rb
@@ -2,10 +2,26 @@ require 'sinatra/base'
 require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions
+
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
+
   get '/bookmarks' do
-    p ENV
+    # p ENV
     @bookmarks = Bookmark.all
-    erb :index
+    erb :"bookmarks/index"
+  end
+
+  post '/bookmarks' do
+    Bookmark.create(url: params[:url], title: params[:title])
+    redirect '/bookmarks'
+  end
+
+  post '/bookmarks' do
+    p params
+    p "Form data submitted to the /bookmarks route!"
   end
 
   # start the server if ruby file executed directly
